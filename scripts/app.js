c8d18c8c1040939f14af30de966c22b332050bcb
chiragns <106506041+craigv4@users.noreply.github.com>
2023-06-30 21:36:26 +0530

    updated how to hide and show overlay where you can update name, also added validation

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,13 @@ const player1EditButton = document.getElementById("edit-player-1-btn");
 const player2EditButton = document.getElementById("edit-player-2-btn");
 const cancelPlayerEdit = document.getElementById("cancel-player-edit");
 
+const formElement = document.querySelector("form");
+const errorText = document.getElementById("error-text");
+
 player1EditButton.addEventListener("click", openEditOverlay);
 player2EditButton.addEventListener("click", openEditOverlay);
 
 cancelPlayerEdit.addEventListener("click", closeEditOverlay);
 backdrop.addEventListener("click", closeEditOverlay);
+
+formElement.addEventListener("submit", updatePlayerName);
--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,19 @@ const openEditOverlay = () => {
 const closeEditOverlay = () => {
 	backdrop.style.display = "none";
 	configOverlay.style.display = "none";
+	formElement.firstElementChild.classList.remove("error");
+	errorText.textContent = "";
+};
+
+const updatePlayerName = (event) => {
+	event.preventDefault(); // Prevents form submission
+	const formData = new FormData(event.target);
+	const playerName = formData.get("player-name").trim(); // Remove whitespace eg. => "   some name     " will return "some name"
+
+	if (!playerName) {
+		// playerName === "", checks if playerName is empty string
+		errorText.textContent = "Player name is invalid";
+		formElement.firstElementChild.classList.add("error");
+		return;
+	}
 };
